5dc234805dcc414146416da1b4e2ecb1be39a10a
HickerDicker <113861320+HickerDicker@users.noreply.github.com>
2024-04-27 08:57:34 +0300

    remove stuff

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let button1 = document.getElementById("button1");
-    let button2 = document.getElementById("button2");
-    let button3 = document.getElementById("button3");
-    let button4 = document.getElementById("button4");
-    let button5 = document.getElementById("button5");
+    const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
+    const button3 = document.getElementById("button3");
+    const button4 = document.getElementById("button4");
+    const button5 = document.getElementById("button5");
     button1.addEventListener("click", function() {
         window.location.href = "index.html";
     });
     button2.addEventListener("click", function() {
         window.location.href = "unsupported.html";
-    });
-    document.addEventListener("DOMContentLoaded", function() {
-        var p2Elements = document.querySelectorAll("p2");
-        p2Elements.forEach(function(element) {
-            element.innerHTML += "<br>";
-        });
     });
     button3.addEventListener("click", function() {
         window.location.href = "OSscreenshots.html";
